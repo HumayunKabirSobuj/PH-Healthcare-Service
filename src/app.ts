715454d6049ae54088044f48c7e2e3af0e351cab
Md. Humayun Kabir Sobuj <humayunkabirgithub@gmail.com>
2025-04-10 00:56:00 +0600

    Project Initialization and Analysis part complete

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
+import { userRoutes } from "./app/modules/user/user.route";
 const app: Application = express();
 app.use(cors());
 
+// parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/v1/user", userRoutes);
 app.get("/", (req: Request, res: Response) => {
   res.send("Server is running");
 });
---
+++ src/app/modules/user/user.service.ts
@@ -0,0 +1,33 @@
+import { PrismaClient, UserRole } from "@prisma/client";
+import bcrypt from "bcrypt";
+
+const prisma = new PrismaClient();
+const createAdmin = async (data: any) => {
+  //   console.log(data);
+  const hashPassword = await bcrypt.hash(data.password, 12);
+
+  const userData = {
+    email: data.admin.email,
+    role: UserRole.ADMIN,
+    passsword: hashPassword,
+  };
+
+  const adminData = data.admin;
+
+  const result = await prisma.$transaction(async (tx) => {
+    await tx.user.create({
+      data: userData,
+    });
+    const createAdminData = await tx.admin.create({
+      data: adminData,
+    });
+
+    return createAdminData;
+  });
+
+  return result;
+};
+
+export const userService = {
+  createAdmin,
+};
