cdb6951f72cbcaa9d53514c07bf510ae4a111381
Md. Humayun Kabir Sobuj <humayunkabirgithub@gmail.com>
2025-04-12 16:57:33 +0600

    Developing Reusable Functions for Prisma Client Constructor and Pagination Calculation Done

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -1,33 +1,10 @@
-import { Prisma, PrismaClient } from "@prisma/client";
+import { Prisma } from "@prisma/client";
 import { adminSearchAbleFields } from "./admin.constant";
+import { paginationHelper } from "../../../helpars/paginationHelper";
+import prisma from "../../../shared/prisma";
 
-const prisma = new PrismaClient();
 const getAllFromDB = async (params: any, options: any) => {
-  
-  const calculatePagination = (options: {
-    page?: number;
-    limit?: number;
-    sortBy?: string;
-    sortOrder?: string;
-  }) => {
-    const page: number = Number(options.page) || 1;
-    const limit: number = Number(options.limit) || 10;
-    const skip: number = (page - 0) * limit;
-    const sortBy: string = options.sortBy || "createdAt";
-    const sortOrder: string = options.sortOrder || "desc";
-    return {
-      page,
-      limit,
-      skip,
-      sortBy,
-      sortOrder,
-    };
-  };
-
-
-
-  const { limit,skip, page } = calculatePagination(options);
-
+  const { limit, skip, page } = paginationHelper.calculatePagination(options);
 
   const { searchTearm, ...filterData } = params;
 
@@ -60,7 +37,7 @@ const getAllFromDB = async (params: any, options: any) => {
   const result = await prisma.admin.findMany({
     where: whereConditions,
     skip,
-    take:limit,
+    take: limit,
     orderBy:
       options.sortBy && options.sortOrder
         ? {
