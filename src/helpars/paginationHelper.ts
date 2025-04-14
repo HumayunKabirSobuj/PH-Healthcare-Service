5947e6e0c938a6d3c48659193878df09f96a6dcb
Md. Humayun Kabir Sobuj <humayunkabirgithub@gmail.com>
2025-04-14 15:11:41 +0600

    part2 start

--- src/helpars/paginationHelper.ts
+++ src/helpars/paginationHelper.ts
@@ -15,7 +15,7 @@ type IOptionsResults = {
 const calculatePagination = (options: IOptions): IOptionsResults => {
   const page: number = Number(options.page) || 1;
   const limit: number = Number(options.limit) || 10;
-  const skip: number = (page - 0) * limit;
+  const skip: number = (page - 1) * limit;
   const sortBy: string = options.sortBy || "createdAt";
   const sortOrder: string = options.sortOrder || "desc";
   return {
