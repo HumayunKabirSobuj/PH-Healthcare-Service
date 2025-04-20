6e931d11b112206c06622fa00ece7f777cafcc40
Md. Humayun Kabir Sobuj <humayunkabirgithub@gmail.com>
2025-04-20 15:07:50 +0600

    Implementing Request Validation Middleware: Part 2 Added

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,32 +1,32 @@
-import { NextFunction, Request, Response } from "express";
-import status from "http-status";
-
-const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  // console.log('error occured...');
-  res.status(status.INTERNAL_SERVER_ERROR).json({
-    success: false,
-    message: err.message || "something went wrong",
-    error: err,
-  });
-};
-
-export default globalErrorHandler;
-
-
-// import { NextFunction, Request, Response } from "express"
+// import { NextFunction, Request, Response } from "express";
 // import status from "http-status";
 
-// const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-//     res.status(status.INTERNAL_SERVER_ERROR).json({
-//         success: false,
-//         message: err.name || "Something went wrong!",
-//         error: err
-//     })
+// const globalErrorHandler = (
+//   err: any,
+//   req: Request,
+//   res: Response,
+//   next: NextFunction
+// ) => {
+//   // console.log('error occured...');
+//   res.status(status.INTERNAL_SERVER_ERROR).json({
+//     success: false,
+//     message: err.message || "something went wrong",
+//     error: err,
+//   });
 // };
 
 // export default globalErrorHandler;
+
+
+import { NextFunction, Request, Response } from "express"
+import status from "http-status";
+
+const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+    res.status(status.INTERNAL_SERVER_ERROR).json({
+        success: false,
+        message: err.name || "Something went wrong!",
+        error: err
+    })
+};
+
+export default globalErrorHandler;
--- src/app/modules/Admin/admin.routes.ts
+++ src/app/modules/Admin/admin.routes.ts
@@ -1,13 +1,33 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { AdminController } from "./admin.controller";
+import { AnyZodObject, z } from "zod";
 
 const router = express.Router();
+
+const update = z.object({
+  body: z.object({
+    name: z.string().optional(),
+    contactNumber: z.string().optional(),
+  }),
+});
+
+const validateRequest = (schema: AnyZodObject) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    // console.log(req.body);
+    try {
+      await schema.parseAsync({ body: req.body });
+      return next();
+    } catch (err) {
+      next(err);
+    }
+  };
+};
 
 router.get("/", AdminController.getAllFromDB);
 
 router.get("/:id", AdminController.getByIdFromDB);
 
-router.patch("/:id", AdminController.updateIntoDB);
+router.patch("/:id", validateRequest(update), AdminController.updateIntoDB);
 
 router.delete("/:id", AdminController.deleteFromDB);
 
