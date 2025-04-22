eac37df94c7bd2aa4fe5d6fff3ec62e261aefeea
Md. Humayun Kabir Sobuj <humayunkabirgithub@gmail.com>
2025-04-22 17:19:51 +0600

    Implementing Authentication Middleware for Role-Based Access on APIs Added

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,8 +1,39 @@
-import express from "express";
+import jwt, { Secret } from "jsonwebtoken";
+import express, { NextFunction, Request, Response } from "express";
 import { userController } from "./user.controller";
+import { UserRole } from "@prisma/client";
+import { jwtHelpers } from "../../../helpars/jwtHelpers";
+import config from "../../../config";
 
 const router = express.Router();
 
-router.post('/', userController.createAdmin)
+const auth = (...roles: string[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = req.headers.authorization;
+      if (!token) {
+        throw new Error("You are not authorized");
+      }
+      const verifiedUser = jwtHelpers.verifyToken(
+        token,
+        config.jwt.jwt_secret as Secret
+      );
+      //   console.log(verifiedUser);
+
+      if (roles.length && !roles.includes(verifiedUser.role)) {
+        throw new Error("You are not authorized");
+      }
+      next();
+    } catch (err) {
+      next(err);
+    }
+  };
+};
+
+router.post(
+  "/",
+  auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
+  userController.createAdmin
+);
 
 export const userRoutes = router;
