a791a61eb0f708ed0a48c2676e1fffc72e2fd561
Md. Humayun Kabir Sobuj <humayunkabirgithub@gmail.com>
2025-04-22 18:21:43 +0600

    Creating Reusable Authentication Middleware and Handling API Errors

---
+++ src/app/middlewares/auth.ts
@@ -0,0 +1,31 @@
+import { NextFunction, Request, Response } from "express";
+import { Secret } from "jsonwebtoken";
+import { jwtHelpers } from "../../helpars/jwtHelpers";
+import config from "../../config";
+import ApiError from "../errors/ApiError";
+import status from "http-status";
+
+const auth = (...roles: string[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = req.headers.authorization;
+      if (!token) {
+        throw new ApiError(status.UNAUTHORIZED, "You are not authorized!");
+      }
+      const verifiedUser = jwtHelpers.verifyToken(
+        token,
+        config.jwt.jwt_secret as Secret
+      );
+      //   console.log(verifiedUser);
+
+      if (roles.length && !roles.includes(verifiedUser.role)) {
+        throw new ApiError(status.FORBIDDEN,"Forbidden!");
+      }
+      next();
+    } catch (err) {
+      next(err);
+    }
+  };
+};
+
+export default auth;
