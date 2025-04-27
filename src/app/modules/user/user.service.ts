daf4dbe96fab17e61eb70c011155bc3f891da252
Md. Humayun Kabir Sobuj <humayunkabirgithub@gmail.com>
2025-04-27 14:22:37 +0600

    Defining Types for File and Cloudinary Response Added

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -2,44 +2,47 @@ import { PrismaClient, UserRole } from "@prisma/client";
 import bcrypt from "bcrypt";
 import prisma from "../../../shared/prisma";
 import { fileUplader } from "../../../helpars/fileUploader";
+import { IFile } from "../../interfaces/file";
 
 const createAdmin = async (req: any) => {
   // console.log("File : ", req.file);
-  console.log("Data : ", req.body.data);
+  console.log("Data : ", req.body);
 
-  const file = req.file;
+  const file:IFile = req.file;
 
   if (file) {
     const uploadToCloudinary = await fileUplader.uploadToCloudinary(file);
-    // console.log({uploadToCloudinary});
-    req.body.data.admin.profilePhoto = (uploadToCloudinary as any)?.secure_url;
+    console.log({ uploadToCloudinary });
+    req.body.admin.profilePhoto = uploadToCloudinary?.secure_url;
 
-    console.log(req.body.data);
+    // console.log(req.body);
   }
 
+  const data = req.body;
+
   //   console.log(data);
-  // const hashPassword = await bcrypt.hash(data.password, 12);
+  const hashPassword = await bcrypt.hash(data.password, 12);
 
-  // const userData = {
-  //   email: data.admin.email,
-  //   role: UserRole.ADMIN,
-  //   passsword: hashPassword,
-  // };
+  const userData = {
+    email: data.admin.email,
+    role: UserRole.ADMIN,
+    passsword: hashPassword,
+  };
 
-  // const adminData = data.admin;
+  const adminData = data.admin;
 
-  // const result = await prisma.$transaction(async (tx) => {
-  //   await tx.user.create({
-  //     data: userData,
-  //   });
-  //   const createAdminData = await tx.admin.create({
-  //     data: adminData,
-  //   });
+  const result = await prisma.$transaction(async (tx) => {
+    await tx.user.create({
+      data: userData,
+    });
+    const createAdminData = await tx.admin.create({
+      data: adminData,
+    });
 
-  //   return createAdminData;
-  // });
+    return createAdminData;
+  });
 
-  // return result;
+  return result;
 };
 export const userService = {
   createAdmin,
--- src/helpars/fileUploader.ts
+++ src/helpars/fileUploader.ts
@@ -1,7 +1,8 @@
 import multer from "multer";
 import path from "path";
 import { v2 as cloudinary } from "cloudinary";
-
+import fs from "fs";
+import { ICloudinaryResponse, IFile } from "../app/interfaces/file";
 // Configuration
 cloudinary.config({
   cloud_name: "dn7oeugls",
@@ -23,16 +24,14 @@ const upload = multer({ storage: storage });
 // Configuration
 // const filePath = path.resolve(__dirname, "../../../../uploads/code-snapshot.png");
 
-const uploadToCloudinary = async (file: any) => {
+const uploadToCloudinary = async (file: IFile):Promise<ICloudinaryResponse | undefined> => {
   // console.log("cloudinary",file);
 
   return new Promise((resolve, reject) => {
     cloudinary.uploader.upload(
       file.path,
-      {
-        public_id: file.originalname,
-      },
-      (error, result) => {
+      (error:Error, result:ICloudinaryResponse) => {
+        fs.unlinkSync(file.path);
         if (error) {
           reject(error);
         } else {
